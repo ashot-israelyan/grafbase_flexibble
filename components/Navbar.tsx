db17f571962874498f69273e6d18b0bd361b527d
Ashot Israelyan <ashot@picsart.com>
2023-09-13 10:56:52 +0400

    Authorization, project create/update implemented

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,11 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { NavLinks } from '@/constants';
 import AuthProviders from '@/components/AuthProviders';
+import { getCurrentUser } from '@/lib/session';
+import ProfileMenu from '@/components/ProfileMenu';
 
-const Navbar = () => {
-  const session = {};
+const Navbar = async () => {
+  const session = await getCurrentUser();
 
   return (
     <nav className="flexBetween navbar">
@@ -21,15 +23,15 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="flexCenter gap-4">
-        {session ? (
+        {session?.user ? (
           <>
-            UserPhoto
+            <ProfileMenu session={session} />
 
             <Link href="/create-project">
               Share Work
             </Link>
           </>
-        ): (
+        ) : (
           <AuthProviders />
         )}
       </div>
---
+++ graphql/index.ts
@@ -0,0 +1,61 @@
+export const createProjectMutation = `
+  mutation CreateProject($input: ProjectCreateInput!) {
+    projectCreate(input: $input) {
+      project {
+        id
+        title
+        description
+        createdBy {
+          email
+          name
+        }
+      }
+    }
+  }
+`;
+
+export const updateProjectMutation = `
+  mutation UpdateProject($id: ID!, $input: ProjectUpdateInput!) {
+    projectUpdate(by: { id: $id }, input: $input) {
+      project {
+        id
+        title
+        description
+        createdBy {
+          email
+          name
+        }
+      }
+    }
+  }
+`;
+
+export const getUserQuery = `
+  query GetUser($email: String!) {
+    user(by: { email: $email }) {
+      id
+      name
+      email
+      avatarUrl
+      description
+      githubUrl
+      linkedInUrl
+    }
+}
+`;
+
+export const createUserMutation = `
+  mutation CreateUser($input: UserCreateInput!) {
+    userCreate(input: $input) {
+      user {
+        id
+        name
+        email
+        avatarUrl
+        description
+        githubUrl
+        linkedInUrl
+      }
+    }
+  }
+`;
